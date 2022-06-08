c3f156163cc92a6f8ed17cd7cc6ea281b3c37715
soxft <email@xcsoft.top>
2022-06-08 14:17:36 +0800

    feta: remove useless route

--- process/webutil/route.go
+++ process/webutil/route.go
@@ -2,7 +2,6 @@ package webutil
 
 import (
 	"busuanzi/app/controller"
-	"busuanzi/app/middleware"
 	"github.com/gin-gonic/gin"
 )
 
@@ -10,13 +9,5 @@ func initRoute(r *gin.Engine) {
 	{
 		r.POST("/api", controller.ApiHandler)
 		r.GET("/ping", controller.PingHandler)
-
-		static := r.Group("/")
-		{
-			static.Use(middleware.Cache())
-			static.GET("/", controller.Index)
-			static.StaticFile("/js", "dist/busuanzi.js")
-		}
-		r.NoRoute(middleware.Cache(), controller.Index)
 	}
 }
